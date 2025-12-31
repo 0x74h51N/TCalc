592a11cdb4a81f012e6637f63a216cb20962ed3f
0x74h51n <tahsin.onemli90@gmail.com>
2025-12-31 16:20:12 +0300

    Core/utils: imag char/alias table

--- src/tcalc/core/constants.py
+++ src/tcalc/core/constants.py
@@ -6,9 +6,12 @@ E: object = calc_native.e
 PI: object = calc_native.pi
 I_UNIT: object = calc_native.i
 
+I_UNIT_CHARS = frozenset({"i", "I", "j", "J"})
+I_UNIT_ALIASES: dict[str, object] = {ch: I_UNIT for ch in I_UNIT_CHARS}
+
 CONSTANTS: dict[str, object] = {
     "e": E,
     "pi": PI,
     "π": PI,
-    "i": I_UNIT,
+    **I_UNIT_ALIASES,
 }
