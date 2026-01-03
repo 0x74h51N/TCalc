5ad2ee0e00e9ea4781e5adc0a2767e781f4144f3
0x74h51n <tahsin.onemli90@gmail.com>
2026-01-03 12:29:42 +0300

    Tests/py: core/utils refactor: test case parametrization

--- tests/py/unit/core/test_utils.py
+++ tests/py/unit/core/test_utils.py
@@ -6,13 +6,18 @@ import pytest
 
 from tcalc.core import utils as utils_mod
 
-
-def test_parse_number_token_int(fake_calc_native):
-    assert utils_mod.parse_number_token("123") == 123
+param = pytest.param
 
 
-def test_parse_number_token_float(fake_calc_native):
-    assert utils_mod.parse_number_token("3.14") == 3.14
+@pytest.mark.parametrize(
+    ("literal", "expected"),
+    [
+        param("123", 123, id="int"),
+        param("3.14", 3.14, id="float"),
+    ],
+)
+def test_parse_number_token_basic(fake_calc_native, literal, expected):
+    assert utils_mod.parse_number_token(literal) == expected
 
 
 def test_parse_number_token_scientific_bigreal(fake_calc_native):
@@ -38,18 +43,18 @@ def test_parse_number_token_scientific_fallback_float(monkeypatch):
 @pytest.mark.parametrize(
     ("literal", "expected"),
     [
-        ("i", 1j),
-        ("I", 1j),
-        ("j", 1j),
-        ("J", 1j),
-        ("3i", 3j),
-        ("i3", 3j),
-        ("3j", 3j),
-        ("j3", 3j),
-        ("-2i", -2j),
-        ("i-2", -2j),
-        ("-2j", -2j),
-        ("j-2", -2j),
+        param("i", 1j, id="i"),
+        param("I", 1j, id="I"),
+        param("j", 1j, id="j"),
+        param("J", 1j, id="J"),
+        param("3i", 3j, id="3i"),
+        param("i3", 3j, id="i3"),
+        param("3j", 3j, id="3j"),
+        param("j3", 3j, id="j3"),
+        param("-2i", -2j, id="-2i"),
+        param("i-2", -2j, id="i-2"),
+        param("-2j", -2j, id="-2j"),
+        param("j-2", -2j, id="j-2"),
     ],
 )
 def test_parse_number_token_imaginary(literal, expected, fake_calc_native):
@@ -60,7 +65,13 @@ def test_parse_number_token_imaginary_scientific(fake_calc_native):
     assert utils_mod.parse_number_token("1e-3i") == 0.001j
 
 
-def test_is_int_like():
-    assert utils_mod.is_int_like(3.0)
-    assert utils_mod.is_int_like(2.0000000000001)
-    assert not utils_mod.is_int_like(2.1)
+@pytest.mark.parametrize(
+    ("value", "expected"),
+    [
+        param(3.0, True, id="int"),
+        param(2.0000000000001, True, id="epsilon-close"),
+        param(2.1, False, id="fractional"),
+    ],
+)
+def test_is_int_like(value, expected):
+    assert utils_mod.is_int_like(value) is expected
