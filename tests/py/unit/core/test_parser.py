6b2e402ee28e8dfe1e19b0001d1678924767d090
0x74h51n <tahsin.onemli90@gmail.com>
2026-01-04 17:24:33 +0300

    Tests/py: test parser format

--- tests/py/unit/core/test_parser.py
+++ tests/py/unit/core/test_parser.py
@@ -8,21 +8,21 @@ from tcalc.core import parser as parser_mod
 param = pytest.param
 
 
-def test_rpn_binary_order(fake_ops, op_ids, token_factory, dummy_calc):
+def test_rpn_binary_order( op_ids, token_factory, dummy_calc):
     num, op = token_factory
     tokens = [num(41), num(69), op(op_ids.SUB)]
 
     assert parser_mod.evaluate_rpn(tokens, dummy_calc) == -28
 
 
-def test_rpn_unary_and_postfix(fake_ops, op_ids, token_factory, dummy_calc):
+def test_rpn_unary_and_postfix( op_ids, token_factory, dummy_calc):
     num, op = token_factory
     tokens = [num(50), op(op_ids.PERCENT), op(op_ids.NEGATE)]
 
     assert parser_mod.evaluate_rpn(tokens, dummy_calc) == -0.5
 
 
-def test_rpn_needs_unit(fake_ops, op_ids, token_factory, dummy_calc, angle_unit):
+def test_rpn_needs_unit( op_ids, token_factory, dummy_calc, angle_unit):
     num, op = token_factory
     tokens = [num(1), op(op_ids.SIN)]
 
@@ -37,7 +37,7 @@ def test_rpn_needs_unit(fake_ops, op_ids, token_factory, dummy_calc, angle_unit)
         param(("num", 69, "op", "ADD"), id="binary-missing-operand"),
     ],
 )
-def test_rpn_malformed_raises(fake_ops, op_ids, token_factory, dummy_calc, rpn):
+def test_rpn_malformed_raises( op_ids, token_factory, dummy_calc, rpn):
     num, op = token_factory
     it = iter(rpn)
     tokens = []
@@ -60,10 +60,10 @@ def test_rpn_malformed_raises(fake_ops, op_ids, token_factory, dummy_calc, rpn):
         param("123", 123, id="number-int"),
     ],
 )
-def test_coerce_token(literal, expected, fake_parse_number):
+def test_coerce_token(literal, expected, ):
     assert parser_mod._coerce_token(literal) == expected
 
 
-def test_coerce_token_invalid_raises(fake_parse_number):
+def test_coerce_token_invalid_raises():
     with pytest.raises(errors.Error):
         parser_mod._coerce_token("si")
